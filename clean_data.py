821fa712c874d8d85e392b23cac892d57eb36108
limunozca <limunozca@unal.edu.co>
2024-03-08 19:56:41 -0500

    Taller4

--- clean_data.py
+++ clean_data.py
@@ -1,20 +1,27 @@
 """Taller evaluable presencial"""
 
+import nltk
 import pandas as pd
 
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
-
-    #
-    # Esta parte es igual al taller anterior
-    #
     df = pd.read_csv(input_file)
     return df
 
 
-def create_key(df, n):
-    """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
+def create_fingerprint(df):
+    """Cree una nueva columna en el DataFrame que contenga el fingerprint de la columna 'text'"""
+
+    # 1. Copie la columna 'text' a la columna 'fingerprint'
+    # 2. Remueva los espacios en blanco al principio y al final de la cadena
+    # 3. Convierta el texto a minúsculas
+    # 4. Transforme palabras que pueden (o no) contener guiones por su version sin guion.
+    # 5. Remueva puntuación y caracteres de control
+    # 6. Convierta el texto a una lista de tokens
+    # 7. Transforme cada palabra con un stemmer de Porter
+    # 8. Ordene la lista de tokens y remueve duplicados
+    # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
 
     df = df.copy()
     df["key"] = df["text"]
@@ -22,35 +29,25 @@ def create_key(df, n):
     df["key"] = df["key"].str.lower()
     df["key"] = df["key"].str.replace("-", "")
     df["key"] = df["key"].str.translate(
-        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~") #eficiente para reemplazar caracteres individuales
+        #reemplace por nulo cadena vacía culaquiera de esas apariciones
     )
-    df["key"] = df["key"].str.split()
+    df["key"] = df["key"].str.split() # va a partir las cadenas de texto en palabras, tenemomos la lista con las palabras
 
-    # ------------------------------------------------------
-    # Esta es la parte especifica del algoritmo de n-gram:
-    #
-    # - Una el texto sin espacios en blanco
-
-    #
-    # - Convierta el texto a una lista de n-gramas
-
-    #
-    # - Ordene la lista de n-gramas y remueve duplicados
-
-    #
-    # - Convierta la lista de ngramas a una cadena
-
-    ## ------------------------------------------------------
-
+    stemmer = nltk.PorterStemmer() 
+    df["key"] =df["key"].apply(lambda x: [stemmer.stem(word) for word in x]) #aplicamos el stemmer a cada palabra de la lista
+    # a cada argumento de la columna x que es la lista de palabras, para cada palabra apliquele el stemmer
+    df["key"] = df["key"].apply(lambda x: sorted(set(x))) #ordena la lista y remueve duplicados, a cada fila, es un conjunto
+    df["key"] = df["key"].str.join(" ") #convierte la lista a cadena de texto separada por espacios, a cada fila
     return df
 
+
+
+#df =create_fingerprint(df)
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
-    #
-    # Este código es identico al anteior
-    #
     df = df.copy()
     df = df.sort_values(by=["key", "text"], ascending=[True, True])
     keys = df.drop_duplicates(subset="key", keep="first")
@@ -59,25 +56,25 @@ def generate_cleaned_column(df):
 
     return df
 
-
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
-    #
-    # Este código es identico al anteior
-    #
+    # Solo contiene una columna llamada 'texto' al igual
+    # que en el archivo original pero con los datos limpios
+
+    """Guarda el DataFrame en un archivo"""
+    # Solo contiene una columna llamada 'texto' al igual
+    # que en el archivo original pero con los datos limpios
+
     df = df.copy()
     df = df[["cleaned"]]
     df = df.rename(columns={"cleaned": "text"})
     df.to_csv(output_file, index=False)
 
-
-def main(input_file, output_file, n=2):
+def main(input_file, output_file):
     """Ejecuta la limpieza de datos"""
-    #
-    # Este código es identico al anteior
-    #
+
     df = load_data(input_file)
-    df = create_key(df, n)
+    df = create_fingerprint(df)
     df = generate_cleaned_column(df)
     df.to_csv("test.csv", index=False)
     save_data(df, output_file)
